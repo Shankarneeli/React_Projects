7f75074f4d5dcd79de48d6c51367c74d8c08e003
Sai Shankar <shankarneelioff@gmail.com>
2025-03-09 13:47:22 +0530

    Added Toggle Card Project

---
+++ toggle_card/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import cc from '../public/cc.png'; 
+import './App.css'
+
+function App() {
+  let [toggle,setToggle] = useState(false);
+
+  function toggleHandler(){
+    console.log("Toggle Button Clicked...");
+    
+    if(toggle == true){
+      setToggle(false);
+    }else{
+      setToggle(true);
+    }
+  }
+  return (
+    <>
+     <div className={toggle == true? "center" : "d-none"}>
+        <img src={cc} className='center-img' alt="Choclate Cake" />
+        <div className="padding">
+        <h3>This is a Toggle Card</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem vitae nostrum numquam maxime dolorem est.adipisicing </p>
+        </div>
+     </div>
+
+      <button className='toggle' onClick={toggleHandler}>Toggle Here !</button>
+    </>
+  )
+}
+
+export default App
